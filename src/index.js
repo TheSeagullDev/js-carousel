dd0643691784d5918c4d7f3e9072ba52126b4969
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2025-05-09 16:43:18 -0400

    Add basic functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import "./styles.css";
+
+const images = document.querySelectorAll("img");
+let index = 0;
+
+function hideAll(imagesArr) {
+  imagesArr.forEach((image) => {
+    image.style.display = "none";
+});
+}
+
+function goTo(imagesArr, index) {
+  hideAll(imagesArr);
+  const img = imagesArr.item(index);
+  img.style.display = "block";
+}
+
+function next(imagesArr) {
+  const length = images.length;
+  index += 1;
+  if (index >= length) {
+    index = 0;
+  }
+  goTo(imagesArr, index);
+}
+
+function last(imagesArr) {
+  const length = images.length;
+  index -= 1;
+  if (index < 0) {
+    index = length - 1;
+  }
+  goTo(imagesArr, index);
+}
+
+const nextBtn = document.createElement("button");
+const lastBtn = document.createElement("button");
+const body = document.querySelector("body");
+nextBtn.textContent = "Next";
+nextBtn.addEventListener("click", () => next(images));
+body.appendChild(nextBtn);
+
+lastBtn.textContent = "Last";
+lastBtn.addEventListener("click", () => last(images));
+body.appendChild(lastBtn);
+
+goTo(images, 0);
+
+setInterval(() => next(images), 5000);
